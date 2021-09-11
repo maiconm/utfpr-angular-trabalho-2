cc6f262fb6b7f4bca105135ae9e46bd03e512bf6
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-09-11 14:24:01 +0000

    Remove placeholders do `AppComponent`.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,17 +22,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-aula01'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-aula01');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-aula01 app is running!');
-  });
-
 });
