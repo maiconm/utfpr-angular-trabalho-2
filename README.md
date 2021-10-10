0f8806e143f69b21dc71f9a9a06687fddeb7c3e3
maiconm <maiconm23@gmail.com>
2021-10-09 22:12:18 -0300

    Renomeia nome do projeto

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularAula02
+# UTFPRAngularTrabalho2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.5.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-aula02'),
+      dir: require('path').join(__dirname, './coverage/utfpr-angular-trabalho-2'),
       subdir: '.',
       reporters: [
         { type: 'html' },
