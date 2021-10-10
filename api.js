25c59eaa1f8e0e93d35d1120f46c6814fdb6460b
maiconm <maiconm23@gmail.com>
2021-10-10 18:21:00 +0000

    Configura a API para habilitar nosso front end para ler a Response HTTP.

--- api.js
+++ api.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const turmasRouter = require('./api/categorias');
 
 const API_PORT = 3001;
 const API_PATH = 'api';
 
 const app = express();
+
+app.use(cors());
 
 app.use(`/${API_PATH}/categorias`, turmasRouter);
 
