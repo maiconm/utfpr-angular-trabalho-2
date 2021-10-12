9d4efad00b183ab801598b1b142bf3542f3a13bd
maiconm <maiconm23@gmail.com>
2021-10-12 21:05:56 +0000

    Cria LivroComponent em branco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriaListaComponent } from './components/categoria-lista/categoria-lista.component';
+import { LivroComponent } from './components/livro/livro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriaListaComponent,
+    LivroComponent,
   ],
   imports: [
     BrowserModule,
