04daf94186b740db960c61dddda1f1a99e116105
maiconm <maiconm23@gmail.com>
2021-10-12 18:45:55 +0000

    Cria interface de livro

---
+++ src/app/interfaces/livro.ts
@@ -0,0 +1,14 @@
+export interface Livro {
+  /**
+   * Id do livro.
+   */
+  _id: string;
+  /**
+   * Título do livro.
+   */
+  titulo: string;
+  /**
+   * Url da capa do livro.
+   */
+  capa: string;
+}
