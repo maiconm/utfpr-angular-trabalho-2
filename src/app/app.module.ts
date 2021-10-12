5757bb8e51740046a7fda55cc24c8135169415b5
maiconm <maiconm23@gmail.com>
2021-10-12 19:18:07 +0000

    Cria componente CategoriaListaComponent em branco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriaListaComponent } from './components/categoria-lista/categoria-lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CategoriaListaComponent,
   ],
   imports: [
     BrowserModule,
