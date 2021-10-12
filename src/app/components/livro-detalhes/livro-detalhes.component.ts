2b1764d69d00c669bf658832ca27d36a74594114
maiconm <maiconm23@gmail.com>
2021-10-12 22:27:19 +0000

    Cria layout do LivroDetalhesComponent

--- src/app/components/livro-detalhes/livro-detalhes.component.ts
+++ src/app/components/livro-detalhes/livro-detalhes.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { LivroDetalhes } from 'src/app/models/livro-detalhes';
 
 @Component({
   selector: 'app-livro-detalhes',
   templateUrl: './livro-detalhes.component.html',
   styleUrls: ['./livro-detalhes.component.css']
 })
-export class LivroDetalhesComponent implements OnInit {
+export class LivroDetalhesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public livroDetalhes = new LivroDetalhes(
+    'teste',
+    'teste',
+    'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTZLjA_xrj12shksuLChiHCAjrptTIcVV1liAdVD2c-yRsuSfux3hvrejETPqw8yTb8FbfXzaYSnViS&usqp=CAc',
+    'als;kfjdlaksdjf as;lkdfjalksdfj;alskdjf a;sldkfj;aslkdfjasdf ;lkjasdf;lkjasd;lfkj ;lkjsdaflk;j',
+    'asdfasdf',
+    'asdfasdf'
+  );
 
 }
