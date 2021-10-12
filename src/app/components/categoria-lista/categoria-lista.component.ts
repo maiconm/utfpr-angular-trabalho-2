cb0ee295712f1994aefd7d201638410be2e6e093
maiconm <maiconm23@gmail.com>
2021-10-12 21:04:54 +0000

    Renderiza Categoria e mocks de filmes

--- src/app/components/categoria-lista/categoria-lista.component.ts
+++ src/app/components/categoria-lista/categoria-lista.component.ts
@@ -1,15 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Categoria } from 'src/app/models/categoria';
+import { Livro } from 'src/app/models/livro';
 
 @Component({
   selector: 'app-categoria-lista',
   templateUrl: './categoria-lista.component.html',
   styleUrls: ['./categoria-lista.component.css']
 })
-export class CategoriaListaComponent implements OnInit {
+export class CategoriaListaComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  categorias = [
+    new Categoria(
+      'fafafafa',
+      'Terror',
+      [
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        ),
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        ),
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        ),
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        )
+      ],
+    ),
+    new Categoria(
+      'fafafafa',
+      'Terror',
+      [
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        )
+      ],
+    ),
+    new Categoria(
+      'fafafafa',
+      'Terror',
+      [
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        ),
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        )
+      ],
+    ),
+    new Categoria(
+      'fafafafa',
+      'Terror',
+      [
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        ),
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        ),
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        ),
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        ),
+        new Livro(
+          'fafafafa',
+          'Unholy',
+          'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/theunholy_keyart_1400x2100_he.png?itok=Jx3Hwj4h',
+        ),
+      ],
+    )
+  ]
 
 }
