0f62ee55c62a1444bb2fc3da7d36996bc1748c4f
maiconm <maiconm23@gmail.com>
2021-10-17 02:00:32 +0000

    Testes de LivroComponent

--- src/app/components/livro/livro.component.spec.ts
+++ src/app/components/livro/livro.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { first } from 'rxjs/operators';
 import { Livro } from 'src/app/models/livro';
 
 import { LivroComponent } from './livro.component';
@@ -29,5 +31,13 @@ describe('LivroComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve emitir o livro correto', () => {
+    const cardDe = fixture.debugElement.query(By.css('.card'));
+    let livroSelecionado: Livro | undefined;
+    component.livroSelecionado.pipe(first()).subscribe(livro => { livroSelecionado = livro })
+    cardDe.triggerEventHandler('click', null);
+    expect(livroSelecionado).toBe(component.livro);
   });
 });
