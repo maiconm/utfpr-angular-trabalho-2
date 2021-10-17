4435208d3d2fcf6670e87b4e8a81db6eadbbf56a
maiconm <maiconm23@gmail.com>
2021-10-17 02:09:44 +0000

    Testes de LivroDetalhesComponent

--- src/app/components/livro-detalhes/livro-detalhes.component.spec.ts
+++ src/app/components/livro-detalhes/livro-detalhes.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { first } from 'rxjs/operators';
 import { LivroDetalhes } from 'src/app/models/livro-detalhes';
 
 import { LivroDetalhesComponent } from './livro-detalhes.component';
@@ -32,5 +34,13 @@ describe('LivroDetalhesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve emitir a ação de fechar ao clicar no botão', () => {
+    const botaoDe = fixture.debugElement.query(By.css('button'));
+    let fechado = false;
+    component.fechar.pipe(first()).subscribe(fechar => { fechado = fechar })
+    botaoDe.triggerEventHandler('click', null);
+    expect(fechado).not.toBeTrue();
   });
 });
